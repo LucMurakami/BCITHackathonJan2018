5f6ca820125d7de66945346041cfbfae1f88de7f
Luc Murakami <luc.murakami@hotmail.com>
2018-01-03 16:03:30 -0800

    Added validation

--- studentValidaion.php
+++ studentValidaion.php
@@ -1,1 +1,35 @@
-<?php ?>
+<?php
+session_start();
+
+$errors = array();
+
+$database = mysqli_connect('localhost', 'root', '',  'registration');
+
+if (isset($_POST['login_user'])) {
+    $loginID = mysqli_real_escape_string($db, $_POST['studentID']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+    
+    if (empty($studentID)) {
+        array_push($errors, "");
+    }
+    if (empty($password)) {
+        array_push($errors, "");
+    }
+    
+    if (count($errors) == 0) {
+        $query = "SELECT * FROM student WHERE studentID='$loginID' AND password='$password'";
+        $results = mysqli_query($db, $query);
+        if (mysqli_num_rows($results == 1)) {
+            $_SESSION['studentID'] = $studentID;
+            header('location: student_index.php');
+        } else if {
+            $query = "SELECT * FROM teacher WHERE teacherID='$loginID' AND password='$password'";
+            $results = mysqli_query($db, $query);
+            if (mysqli_num_rows($results == 1)) {
+                $_SESSION['studentID'] = $studentID;
+                header('location: student_index.php');
+        }
+    }
+}
+?>
+    
